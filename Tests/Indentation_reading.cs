54d3ca30a153fae89b1442052b7935ae466ba4d3
Oscar Marcusson <62374937+OscarMarcusson@users.noreply.github.com>
2023-12-16 08:38:07 +0100

    Add indentation of utility

---
+++ Tests/Indentation_reading.cs
@@ -0,0 +1,54 @@
+using whitespaceNET;
+
+namespace WhitespaceReader
+{
+	[TestClass]
+	public class IndentationOf
+	{
+		[TestMethod]
+		public void Empty()
+		{
+			var result = whitespaceNET.WhitespaceReader.IndentationOf("");
+			Assert.AreEqual(0, result);
+		}
+
+		[TestMethod]
+		public void None()
+		{
+			var result = whitespaceNET.WhitespaceReader.IndentationOf("Hello World");
+			Assert.AreEqual(0, result);
+		}
+
+		[TestMethod]
+		public void Spaces()
+		{
+			var indentSize = 4;
+			for(int i = 1; i < 10; i++)
+			{
+				var whitespace = new string(' ', i * indentSize);
+				var result = whitespaceNET.WhitespaceReader.IndentationOf($"{whitespace}Hello World", indentSize);
+				Assert.AreEqual(i, result);
+			}
+		}
+
+		[TestMethod]
+		public void Tabs()
+		{
+			var indentSize = 4;
+			for (int i = 1; i < 100; i++)
+			{
+				var whitespace = new string('\t', i);
+				var result = whitespaceNET.WhitespaceReader.IndentationOf($"{whitespace}Hello World", indentSize);
+				Assert.AreEqual(i, result);
+			}
+		}
+
+		[TestMethod]
+		public void Mixed()
+		{
+			var indentSize = 4;
+			var result = whitespaceNET.WhitespaceReader.IndentationOf($"    \t    Hello World", indentSize);
+			Assert.AreEqual(3, result);
+		}
+	}
+}
---
+++ whitespaceNET/WhitespaceReader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace whitespaceNET
+{
+	public static class WhitespaceReader
+	{
+		public static int defaultIndentSize = 4;
+
+		public static int IndentationOf(string text, int? indentSize = null)
+		{
+			if (!indentSize.HasValue)
+				indentSize = Math.Max(1, defaultIndentSize);
+
+
+			var indent = 0;
+			for (int i = 0; i < text.Length; i++)
+			{
+				switch (text[i])
+				{
+					case '\t': indent += indentSize.Value; break;
+					case ' ': indent++; break;
+					default:
+						return indent / indentSize.Value;
+				}
+			}
+			return indent / indentSize.Value;
+		}
+	}
+}
